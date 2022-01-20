a033b1067a790c31c25b2f157b86bf0395631613
Dan Stark <danstarkdevops@gmail.com>
2022-01-20 00:15:31 -0500

    Fixes networking issues, some minor typos
    Both commands in the README should work. The main issue was not a docker networking issue (surprisingly), but rather a conf in the test/mainnet containers that block non-local networking access (non-surprising, from a security perspective).
    
    As a dev user, you should be able to run the README commands and connect to the interface server with OCI runtime exec failed: exec failed: container_linux.go:380: starting container process caused: exec: "base": executable file not found in $PATH: unknown and successfully reach the algo node  with  and get a response

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tornado.ioloop
 import tornado.web
-from AlgorandNodeInterfaceBackend import AlgorandNodeIntefaceBackend
+from AlgorandNodeInterfaceBackend import AlgorandNodeInterfaceBackend
 from AlgorandNodeInterfaceBackendHandler import AlgorandNodeInterfaceBackendHandler
 from tornado.httpserver import HTTPServer
 
@@ -8,7 +8,7 @@ def make_app(backend):
     return tornado.web.Application([(r"/", AlgorandNodeInterfaceBackendHandler, {"backend" : backend}), ])
 
 def algorand_node_interface_main(portnumber=42069):
-    backend = AlgorandNodeIntefaceBackend()
+    backend = AlgorandNodeInterfaceBackend()
     http_server = tornado.httpserver.HTTPServer(make_app(backend))
     http_server.bind(portnumber)
     http_server.start()
